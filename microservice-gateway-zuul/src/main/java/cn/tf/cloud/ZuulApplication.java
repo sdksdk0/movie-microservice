4fab879244a8a7960d7fbeef95dea5d85aa3f2a7
朱培 <690644511@qq.com>
2019-06-15 16:41:41 +0800

    zuul聚合请求

--- microservice-gateway-zuul/src/main/java/cn/tf/cloud/ZuulApplication.java
+++ microservice-gateway-zuul/src/main/java/cn/tf/cloud/ZuulApplication.java
@@ -3,8 +3,10 @@ package cn.tf.cloud;
 import cn.tf.cloud.filter.PreRequestLogFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -17,5 +19,11 @@ public class ZuulApplication {
   public PreRequestLogFilter preRequestLogFilter(){
     return new PreRequestLogFilter();
   }
+  @Bean
+  @LoadBalanced
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
+
 
 }
